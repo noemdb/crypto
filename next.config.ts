579ad9ca269b6134cb798f853bf7999818f79927
nuserLatitude <noemdb@gmail.com>
2026-05-04 23:08:20 -0400

    wip 2026-05-04_23:08:20

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["bcryptjs"],
-  transpilePackages: ["@react-email/components"],
 };
 
 export default nextConfig;
